57b36b0122abae5050f37ab0cda4191ead7aee3a
Melanie Kramer <melaniekramer.1990@gmail.com>
2021-02-16 13:13:28 -0500

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Melanie Kramer on 2/16/21.
+//  Copyright © 2021 Melanie Kramer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
