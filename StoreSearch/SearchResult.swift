c0c374365e9ee05a5b768fbecfe00485ecf4444d
Melanie Kramer <melaniekramer.1990@gmail.com>
2021-02-18 21:06:27 -0500

    commit pg 882

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Melanie Kramer on 2/18/21.
+//  Copyright © 2021 Melanie Kramer. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
